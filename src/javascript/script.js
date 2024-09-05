52862a2cdaefc3590992fe3dca8ddee4150b55ea
“nmarcotulio” <marcotulionogueira@gmail.com>
2024-09-05 15:07:18 -0300

    header box-shadow animation

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,20 @@
-$(document).ready(function(){
-    $('.mobile-btn').on('click', function (){
-        $('#mobile-menu').toggleClass('active');
-        $('.mobile-btn').find('i').toggleClass('fa-x');
-    });
-});
+$(document).ready(function () {
+  $(".mobile-btn").on("click", function () {
+    $("#mobile-menu").toggleClass("active")
+    $(".mobile-btn").find("i").toggleClass("fa-x")
+  })
+
+  const sections = $("section")
+  const navItems = $(".nav-item")
+
+  $(window).on("scroll", function () {
+    const header = $("header")
+    const scrollPosition = $(window).scrollTop() - header.outerHeight()
+
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none")
+    } else {
+      header.css("box-shadow", "5px 1px 5px rgba(0, 0, 0, 0.1)")
+    }
+  })
+})
